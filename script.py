82a0af3ecc0e14eb5f58816450a80fe467d1a112
DaPutzy <9727551+DaPutzy@users.noreply.github.com>
2025-12-02 19:59:30 +0100

    rework due to different monitor index in wmi vs monitorcontrol

--- script.py
+++ script.py
@@ -1,7 +1,6 @@
+from retry import retry
 import monitorcontrol
 import pynput
-import pythoncom
-import wmi
 
 ########################
 #      CONSTANTS       #
@@ -23,67 +22,55 @@ VCP_CODE_VALUES = [48, 49, 50, 51]
 KEY_INCREASE = pynput.keyboard.Key.page_up
 KEY_DECREASE = pynput.keyboard.Key.page_down
 
-MANUFACTURER = "DEL" # somehow its DEL not DELL?
-MODEL        = "AW3225QF"
-
-########################
-#    GLOBAL VALUES     #
-########################
-
-current_value_index = 0
-
-########################
-#      FUNCTIONS       #
-########################
-
-def to_string(value):
-    return "".join(chr(c) for c in value if c > 0)
-
-def find_monitor_index():
-    w = wmi.WMI(namespace='root\\wmi')
-
-    for index, info in enumerate(w.WmiMonitorID(), 0):
-        # print(f"ascii: {to_string(info.ManufacturerName)}")
-        # print(f"ascii: {to_string(info.UserFriendlyName)}")
-
-        if info.Active == True and to_string(info.ManufacturerName) == MANUFACTURER and to_string(info.UserFriendlyName) == MODEL:
-            return index
-
-    return -1
-
-# noinspection PyProtectedMember
-def on_press(key):
-    global current_value_index
-
-    pythoncom.CoInitialize()
-
-    if not key == KEY_INCREASE and not key == KEY_DECREASE:
-        return
-
-    monitor_index = find_monitor_index()
-
-    if monitor_index == -1:
-        print("could not find monitor")
-        exit(1)
-
-    monitor = monitorcontrol.get_monitors()[monitor_index]
-    with monitor:
-        if key == KEY_INCREASE:
-            current_value_index = 0 if current_value_index == 3 else current_value_index + 1
-
-        if key == KEY_DECREASE:
-            current_value_index = 3 if current_value_index == 0 else current_value_index - 1
-
-        monitor._set_vcp_feature(VCP_CODE, VCP_CODE_VALUES[current_value_index])
+MODEL = "AW3225QF"
 
 ########################
 #         MAIN         #
 ########################
 
 class Main:
+
+    vcp_code_index = 0
+
+    monitor = None
+
+    # sometimes get_vcp_capabilities fails for no reason
+    @retry(tries=5, delay=5)
+    def get_monitor(self):
+        for monitor in monitorcontrol.get_monitors():
+            with monitor:
+                capabilities = monitor.get_vcp_capabilities()
+
+                if capabilities.get("model") == MODEL:
+                    return monitor
+
+        return None
+
     def __init__(self):
-        with pynput.keyboard.Listener(on_press=on_press) as listener:
+        self.monitor = self.get_monitor()
+
+        if self.monitor is None:
+            print("could not find monitor")
+            exit(1)
+
+        print("ready")
+
+        with pynput.keyboard.Listener(on_press=self.on_press) as listener:
             listener.join()
+
+    # noinspection PyProtectedMember
+    def on_press(self, key):
+        if not key == KEY_INCREASE and not key == KEY_DECREASE:
+            return
+
+        with self.monitor:
+            if key == KEY_INCREASE:
+                self.vcp_code_index = 0 if self.vcp_code_index == 3 else self.vcp_code_index + 1
+
+            if key == KEY_DECREASE:
+                self.vcp_code_index = 3 if self.vcp_code_index == 0 else self.vcp_code_index - 1
+
+            self.monitor._set_vcp_feature(VCP_CODE, VCP_CODE_VALUES[self.vcp_code_index])
 
 if __name__ == "__main__":
     Main()
